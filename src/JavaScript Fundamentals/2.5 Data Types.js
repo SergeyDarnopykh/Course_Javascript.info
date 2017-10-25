b49c6d4ca50bd976a8d0b154ac9d58eaf39393ec
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2017-10-26 01:39:18 +0300

    Finished 2.5 Data Structures

---
+++ src/JavaScript Fundamentals/2.5 Data Types.js
@@ -0,0 +1,14 @@
+/*
+What is the output of the script?
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+alert( `hello ${"name"}` ); // ?
+alert( `hello ${name}` ); // ?
+*/
+
+/*
+hello 1
+hello name
+hello Ilya
+*/
--- src/JavaScript Fundamentals/REAME.md
+++ src/JavaScript Fundamentals/REAME.md
@@ -3,3 +3,5 @@
 If we know the value of the constant before runtime - it should be
 uppercase. Otherwise - camelCase. <br>
 
+The script cannot "die" because of Math. The worst we can get is NaN. <br>
+
